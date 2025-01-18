32730a688b3e119d069ff5c8844515ec34850020
MarcosChavez11 <mchavezarrieta2011@gmail.com>
2025-01-17 19:07:44 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import discord
 from bot_logic import gen_pass
+from bot_logic import flip_coin
+from bot_logic import gen_emodji
 
 # La variable intents almacena los privilegios del bot
 intents = discord.Intents.default()
@@ -16,13 +18,15 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-    if message.content.startswith("hello"):
-        await message.channel.send("Hi!")
+    if message.content.startswith("hola"):
+        await message.channel.send("Hola")
     elif message.content.startswith("bye"):
         await message.channel.send("\U0001f642")
-    elif message.content.startswith("gen"):
+    elif message.content.startswith('+emoji'):
+        await message.channel.send(gen_emodji())
+    elif message.content.startswith('+coin'):
+        await message.channel.send(flip_coin())
+    elif message.content.startswith("+pswd"):
         await message.channel.send(f"Tu contraseña generada es: {gen_pass(10)}")
     else:
-        await message.channel.send(message.content)      
-
-client.run("Aquí va el token de tu bot")
+        await message.channel.send(message.content)
